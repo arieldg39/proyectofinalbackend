2260a30ba16c1afc07259468d62f635cb7e0d387
AlexiaTonin <atoninelias@gmail.com>
2023-01-20 09:47:19 -0300

    Creado controladores, modelo y rutas de los productos

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,14 +1,38 @@
 const { model, Schema, default: mongoose } = require('mongoose');
 
 const ProductSchema = new Schema({
+    category: {
+        type: String,
+        require: true,
+    },
+    image: {
+        type: String,
+        require: true,
+    },
     name: {
         type: String,
+        require: true,
     },
     brand: {
         type: String,
+        require: true,
+    },
+    description: {
+        type: String,
+        require: true,
     },
     price: {
         type: Number,
+        require: true,
+    },
+    hotItem: {
+        type: Boolean,
+        require: true,
+        default:false,
+    },
+    stock: {
+        type: Number,
+        require: true,
     },
     type: {
         type: mongoose.Schema.Types.ObjectId,
