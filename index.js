0f509e46a59ad0330d050d405a5b7ac95cd6173f
AlexiaTonin <atoninelias@gmail.com>
2023-01-21 21:44:57 -0300

    complete base de datos de productos

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
-const mongoose= require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+dotenv.config();
+const cors = require("cors");
+const products = require("./routes/products");
 
-app.use(express.json({ extended: true }));
-app.use(express.urlencoded());
+const connectDb = require("./Db/connectDb");
+connectDb();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use("/products", products);
 
-app.use('/api', require('./src/routes'));
-
-mongoose.connect(process.env.DB_URL).then(()=>{
-    console.log('Conectado al Mongo');
-    server = app.listen(process.env.API_PORT,(req, res)=> {
-        console.log(`Aplicacion corriendo en el puerto ${process.env.API_PORT}!`)
-    });    
+app.get("/", (req, res) => {
+  res.json({ message: "Backend encontrado" });
 });
+
+app.listen(4000, () => console.log("Servidor funcionando en puerto 4000"));
