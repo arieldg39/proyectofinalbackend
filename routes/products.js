61f4d2b43358141202adf38b553410e5c06431fe
AlexiaTonin <atoninelias@gmail.com>
2023-01-31 20:09:50 -0300

    creado ruta para productos destacados

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { getProducts, addProduct, editProduct, deleteProduct } = require("../controllers/products");
+const { getProducts, addProduct, editProduct, deleteProduct, getHotItem } = require("../controllers/products");
 const { deleteProductsMiddleware, addProductMiddleware } = require('../middlewares/products')
 const router = Router();
 
 router.get("/products", getProducts);
+router.get("/hotItem", (req,res,next) => {console.log('ruta correcta'); next()}, getHotItem);
 router.post("/addProducts", addProductMiddleware,addProduct);
 router.put("/editProducts:id", editProduct)
 router.delete("/deleteProducts", deleteProductsMiddleware , deleteProduct);
